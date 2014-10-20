7aa7a564d65b1aae2548581e61fe3d883f7bdbfd
Chip Self <chipself9@gmail.com>
2014-10-20 22:10:42 +0000

    added mutate.rb, say.rb, scope.rb

---
+++ say.rb
@@ -0,0 +1,18 @@
+# say.rb
+
+puts "hello"
+puts "hi"
+puts "how are you"
+puts "I'm fine"
+
+
+
+
+def say(words='hello')
+    puts words + '.'
+end
+
+say()
+say("Hi")
+say("how are you")
+say("I'm fine")

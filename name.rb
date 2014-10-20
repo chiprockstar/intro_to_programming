d0b4543e34fc3931f02e9c04005cb26abddffdd2
Chip Self <chipself9@gmail.com>
2014-10-20 21:02:21 +0000

    refactored name.rb

--- name.rb
+++ name.rb
@@ -1,5 +1,16 @@
 # name.rb
 
+#1
+puts "Please type your name"
+
+name = gets.chomp + " It's nice to meet you"
+
+
+#2
+10.times  { puts name }
+
+
+#3
 puts "Please enter your first name"
 first_name = gets.chomp
 
@@ -8,4 +19,3 @@ last_name = gets.chomp
 
 puts first_name + " " + last_name
 
-10.times  { puts first_name + " " + last_name }

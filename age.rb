9d2304f7bcd24dbc6b6ad4b067ff8de797aa3d5d
Chip Self <chipself9@gmail.com>
2014-10-20 20:44:50 +0000

    age.rb exercise

---
+++ age.rb
@@ -0,0 +1,13 @@
+#age.rb
+
+puts "How old are you?"
+age = gets.chomp
+increment = 10
+
+4.times do |n|
+
+ puts "In " + increment.to_s +  " years you will be:"
+ old_age = increment + age.to_i
+ puts old_age
+ increment = increment + 10
+end

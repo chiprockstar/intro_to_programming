133e3db0c58d105048837113b813949bc7fed4e9
Chip Self <chipself9@gmail.com>
2014-10-23 18:16:09 -0400

    hash excersise 3

--- hash_exercises.rb
+++ hash_exercises.rb
@@ -2,23 +2,43 @@
 
 #2
 
-a1 = { a: 100, b: 345, c: 499, d: 25, e:90 }
-a2 = { d: 75, e: 45, f: 234, g: 24, h:36 }
+# a1 = { a: 100, b: 345, c: 499, d: 25, e:90 }
+# a2 = { d: 75, e: 45, f: 234, g: 24, h:36 }
+#
+#
+#
+# a3 = a1.merge(a2)
+#   puts "old val: #{a1}"      #  old val: {:a=>100, :b=>345, :c=>499, :d=>25, :e=>90}
+#   puts "new val: is #{a3}"   #  new val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
+#   # Without Bang - only writes over if the old symbol does not match an existing new symbol
+#
+#
+# a1 = { a: 100, b: 345, c: 499, d: 25, e:90 }
+# a2 = { d: 75, e: 45, f: 234, g: 24, h:36 }
+#
+#
+#
+# a3 = a1.merge!(a2)
+# puts "old val: #{a1}"      #  old val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
+# puts "new val: is #{a3}"   #  new val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
+#    # Bang - writes over all symbols regardless.
 
 
 
-a3 = a1.merge(a2)
-  puts "old val: #{a1}"      #  old val: {:a=>100, :b=>345, :c=>499, :d=>25, :e=>90}
-  puts "new val: is #{a3}"   #  new val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
-  # Without Bang - only writes over if the old symbol does not match an existing new symbol
+#3
+
+bike = {tires: "18 inch", seat: true, sprocket: false, handle_bars: true }
+
+bike.each_key do |key|
+  puts "Key #{key}"
+end
 
 
-a1 = { a: 100, b: 345, c: 499, d: 25, e:90 }
-a2 = { d: 75, e: 45, f: 234, g: 24, h:36 }
+bike.each_value do |value|
+  puts "Value #{value}"
+end
 
 
-
-a3 = a1.merge!(a2)
-puts "old val: #{a1}"      #  old val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
-puts "new val: is #{a3}"   #  new val: {:a=>100, :b=>345, :c=>499, :d=>75, :e=>45, :f=>234, :g=>24, :h=>36}
-   # Bang - writes over all symbols regardless.
+bike.each do |key, value|
+  puts "Key #{key} Value #{value}"
+end

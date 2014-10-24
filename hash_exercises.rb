f9f2856205ede2a4b54e6578231ee11776090067
Chip Self <chipself9@gmail.com>
2014-10-24 15:23:23 -0400

    hash exercises

--- hash_exercises.rb
+++ hash_exercises.rb
@@ -27,18 +27,96 @@
 
 #3
 
-bike = {tires: "18 inch", seat: true, sprocket: false, handle_bars: true }
+# bike = {tires: "18 inch", seat: true, sprocket: false, handle_bars: true }
+#
+# bike.each_key do |key|
+#   puts "Key #{key}"
+# end
+#
+#
+# bike.each_value do |value|
+#   puts "Value #{value}"
+# end
+#
+#
+# bike.each do |key, value|
+#   puts "Key #{key} Value #{value}"
+# end
 
-bike.each_key do |key|
-  puts "Key #{key}"
-end
 
 
-bike.each_value do |value|
-  puts "Value #{value}"
-end
 
 
-bike.each do |key, value|
-  puts "Key #{key} Value #{value}"
-end
+
+#4
+
+# person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+#
+# puts person.fetch(:name)
+
+
+
+
+
+#5
+
+# has_value?
+#
+#  person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+#
+#
+# def has_value(person, val)
+#   if person.has_value?(val)
+#     puts "Value has been found"
+#   else
+#     puts "Value not found"
+#   end
+# end
+#
+# has_value(person, 'Bob')
+
+
+
+
+
+#6
+
+# I need to review this exercise
+#
+# words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+#           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+#           'flow', 'neon']
+# result = {}
+#
+# words.each do |word|
+#   key = word.split('').sort.join
+#
+#   if result.has_key?(key)
+#     result[key].push(word)
+#   else
+#     result[key] = [word]
+#   end
+#
+# end
+# #
+#  puts result
+# result.each do |k, v|
+#   puts "-----"
+#   p v
+# end
+
+# words.each do |word|
+#   puts word.sort
+# end
+
+
+
+#7
+
+# x: is a symbol
+# x is a loc and used x as the key
+
+
+#8
+
+B.

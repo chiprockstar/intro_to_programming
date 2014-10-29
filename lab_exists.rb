c0511f46bf6a13baff1a4814c642ac3176089016
Chip Self <chipself9@gmail.com>
2014-10-29 20:13:44 +0000

    exception handling excersises

---
+++ lab_exists.rb
@@ -0,0 +1,39 @@
+
+
+# def has_a_lab?(string)
+#   if /lab/.match(string)
+#     puts "We have a match! #{string}"
+#   else
+#     puts "No match here."
+#   end
+# end
+
+# has_a_lab?("laboratory")
+# has_a_lab?("experiment")
+# has_a_lab?("Pans Labyrinth")
+# has_a_lab?("elaborate")
+# has_a_lab?("polar bear")
+
+
+
+# def execute(&block)
+#   block
+# end
+
+# execute { puts "Hello from inside the execute method!" }
+
+# prints nothing no .call method
+
+
+# Exception handling catches errors in invald data and gracefully continues running.
+
+
+# def execute(&block)
+#   block.call
+# end
+
+# execute { puts "Hello from inside the execute method!" }
+
+# the ampersand is missing from the argument indicating a block so no parmaters are passed in.
+
+

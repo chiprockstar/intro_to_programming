036a56c1540860aa4b7154b9e0d388a0d0910c11
Chip Self <chipself9@gmail.com>
2014-10-29 16:15:56 -0400

    various proc and exception handling

--- age.rb
+++ age.rb
@@ -5,9 +5,8 @@ age = gets.chomp
 increment = 10
 
 4.times do |n|
-
- puts "In " + increment.to_s +  " years you will be:"
- old_age = increment + age.to_i
- puts old_age
- increment = increment + 10
+   puts "In " + increment.to_s +  " years you will be:"
+   old_age = increment + age.to_i
+   puts old_age
+   increment = increment + 10
 end
